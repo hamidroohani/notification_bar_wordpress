0c3599019ca26a794002e0c7d0062a6e9ccd4cea
Hamid <hamid.dev@yahoo.com>
2019-06-04 17:11:14 +0430

    123

--- inc/core/kernel.php
+++ inc/core/kernel.php
@@ -5,11 +5,65 @@
  * Date: 6/3/2019
  * Time: 9:49 PM
  */
+$plugin_path = plugin_dir_url(__FILE__);
 ?>
 <div class="admin-n-b-w">
-    <div id="demo-is-here-n-b-w"></div>
+    <div id="demo-is-here-n-b-w">
+        <div class="n-b-w-f-body n-b-w-f-num-01 selected-n-b-w" style="display: none">
+            <div class="n-b-w-f-right">
+                <div class="n-b-w-f-body-per">
+                    <span class="n-b-w-f-per" id="percent-parent"><span id="percent">50</span> % تخفیف</span>
+                    <img src="<?= $plugin_path ?>../template/images/2980.jpg" alt="">
+                </div>
+                <div class="n-b-w-f-title" id="title-product">محصول دارای تخفیف</div>
+                <div class="n-b-w-f-body-price">
+                    <span class="n-b-w-f-price" id="price">39,000 تومان</span>
+                </div>
+            </div>
+            <div class="n-b-w-f-left">
+                <span class="n-b-w-f-button" id="button-title">خرید محصول</span>
+            </div>
+        </div>
+        <div class="n-b-w-f-body n-b-w-f-num-02" style="display: none">
+            <div class="n-b-w-f-right">
+                <div class="n-b-w-f-title" id="title-product">تایمر ها را با طراحی دلخواه ایجاد کنید (تاریخ شمسی)</div>
+            </div>
+            <div class="n-b-w-f-left">
+                <div class="n-b-w-f-counter">
+                    <div class="n-b-w-f-days n-b-w-f-counter-panel">
+                        <span class="n-b-w-f-int">23</span>
+                        <span class="n-b-w-f-str">روز</span>
+                    </div>
+                    <div class="n-b-w-f-hours n-b-w-f-counter-panel">
+                        <span class="n-b-w-f-int">11</span>
+                        <span class="n-b-w-f-str">ساعت</span>
+                    </div>
+                    <div class="n-b-w-f-minuet n-b-w-f-counter-panel">
+                        <span class="n-b-w-f-int">08</span>
+                        <span class="n-b-w-f-str">دقیقه</span>
+                    </div>
+                    <div class="n-b-w-f-second n-b-w-f-counter-panel">
+                        <span class="n-b-w-f-int">23</span>
+                        <span class="n-b-w-f-str">ثانیه</span>
+                    </div>
+                </div>
+                <span class="n-b-w-f-button" id="button-title">تست</span>
+            </div>
+        </div>
+        <div class="n-b-w-f-body n-b-w-f-num-03" style="display: none">
+            <div class="n-b-w-f-center">
+                <div class="n-b-w-f-title" id="title-product">ما را در شبکه های اجتماعی دنبال کنید :</div>
+                <a href="" id="facebook" style="display: none"><img src="<?= $plugin_path ?>../template/images/facebook.svg" alt=""></a>
+                <a href="" id="telegram" style="display: none"><img src="<?= $plugin_path ?>../template/images/telegram.svg" alt=""></a>
+                <a href="" id="youtube" style="display: none"><img src="<?= $plugin_path ?>../template/images/youtube.svg" alt=""></a>
+                <a href="" id="twitter" style="display: none"><img src="<?= $plugin_path ?>../template/images/twitter.svg" alt=""></a>
+                <a href="" id="instagram" style="display: none"><img src="<?= $plugin_path ?>../template/images/instagram.svg" alt=""></a>
+            </div>
+        </div>
+    </div>
     <form action="">
-        <select name="selectNotification" id="">
+
+        <select name="selectNotification" class="fenix-wp-fenix-form-control" id="">
             <option value="introduce_product">معرفی محصول</option>
             <option value="day_counter">روز شمار</option>
             <option value="social">شبکه های اجتماعی</option>
@@ -19,7 +73,28 @@
             <input type="text" class="fenix-wp-fenix-form-control" id='n-b-w-f-title-input' name="title-product">
         </div>
         <div class="n-b-w-percent">
+            <label for="">تخفیف</label>
             <input type="number" class="fenix-wp-fenix-form-control" name="percent">
+        </div>
+        <div class="n-b-w-price">
+            <label for="">قیمت</label>
+            <input type="text" class="fenix-wp-fenix-form-control" name="price">
+        </div>
+        <div class="n-b-w-button-title">
+            <label for="">متن دکمه</label>
+            <input type="text" class="fenix-wp-fenix-form-control" name="button-title">
+        </div>
+        <div class="n-b-w-social" style="display: none">
+            <label for="">تلگرام</label>
+            <input type="text" class="fenix-wp-fenix-form-control" placeholder="آی دی خود را وارد کنید" name="telegram"><br>
+            <label for="">اینستاگرام</label>
+            <input type="text" class="fenix-wp-fenix-form-control" placeholder="آی دی خود را وارد کنید" name="instagram"><br>
+            <label for="">فیسبوک</label>
+            <input type="text" class="fenix-wp-fenix-form-control" placeholder="آی دی خود را وارد کنید" name="facebook"><br>
+            <label for="">توئیتر</label>
+            <input type="text" class="fenix-wp-fenix-form-control" placeholder="آی دی خود را وارد کنید" name="twitter"><br>
+            <label for="">یوتیوب</label>
+            <input type="text" class="fenix-wp-fenix-form-control" placeholder="آدرس کانال خود را وارد کنید" name="youtube"><br>
         </div>
     </form>
 </div>
--- inc/template/js/index.js
+++ inc/template/js/index.js
@@ -3,78 +3,117 @@ jQuery(document).ready(function ($) {
     // get body variable
     let body = $("body");
 
-    async function fillInputs(){
-        let title =await $("#n-b-w-f-title").val();
-        console.log(title);
-        $("#n-b-w-f-title-input").val(title);
-    }
-
-    let notif01 = "<div class=\"n-b-w-f-body n-b-w-f-num-01\">\n" +
-        "    <div class=\"n-b-w-f-right\">\n" +
-        "        <div class=\"n-b-w-f-body-per\">\n" +
-        "            <span class=\"n-b-w-f-per\">50% تخفیف</span>\n" +
-        "            <img src=\"" + nbwSetting + "../template/images/2980.jpg\" alt=\"\">\n" +
-        "        </div>\n" +
-        "        <div class=\"n-b-w-f-title\" id='n-b-w-f-title'>محصول دارای تخفیف</div>\n" +
-        "        <div class=\"n-b-w-f-body-price\">\n" +
-        "            <span class=\"n-b-w-f-price\">39,000 تومان</span>\n" +
-        "        </div>\n" +
-        "    </div>\n" +
-        "    <div class=\"n-b-w-f-left\">\n" +
-        "        <span class=\"n-b-w-f-button\">خرید محصول</span>\n" +
-        "    </div>\n" +
-        "</div>\n";
-    let notif02 = "<div class=\"n-b-w-f-body n-b-w-f-num-02\">\n" +
-        "    <div class=\"n-b-w-f-right\">\n" +
-        "        <div class=\"n-b-w-f-title\">تایمر ها را با طراحی دلخواه ایجاد کنید (تاریخ شمسی)</div>\n" +
-        "    </div>\n" +
-        "    <div class=\"n-b-w-f-left\">\n" +
-        "        <div class=\"n-b-w-f-counter\">\n" +
-        "            <div class=\"n-b-w-f-days n-b-w-f-counter-panel\">\n" +
-        "                <span class=\"n-b-w-f-int\">23</span>\n" +
-        "                <span class=\"n-b-w-f-str\">روز</span>\n" +
-        "            </div>\n" +
-        "            <div class=\"n-b-w-f-hours n-b-w-f-counter-panel\">\n" +
-        "                <span class=\"n-b-w-f-int\">11</span>\n" +
-        "                <span class=\"n-b-w-f-str\">ساعت</span>\n" +
-        "            </div>\n" +
-        "            <div class=\"n-b-w-f-minuet n-b-w-f-counter-panel\">\n" +
-        "                <span class=\"n-b-w-f-int\">08</span>\n" +
-        "                <span class=\"n-b-w-f-str\">دقیقه</span>\n" +
-        "            </div>\n" +
-        "            <div class=\"n-b-w-f-second n-b-w-f-counter-panel\">\n" +
-        "                <span class=\"n-b-w-f-int\">23</span>\n" +
-        "                <span class=\"n-b-w-f-str\">ثانیه</span>\n" +
-        "            </div>\n" +
-        "        </div>\n" +
-        "        <span class=\"n-b-w-f-button\" id='test'>تست</span>\n" +
-        "    </div>\n" +
-        "</div>\n";
-
-    let notif03 = "<div class=\"n-b-w-f-body n-b-w-f-num-03\">\n" +
-        "    <div class=\"n-b-w-f-center\">\n" +
-        "        <div class=\"n-b-w-f-title\">ما را در شبکه های اجتماعی دنبال کنید :</div>\n" +
-        "        <a href=\"\"><img src=\"" + nbwSetting + "../template/images/facebook.svg\" alt=\"\"></a>\n" +
-        "        <a href=\"\"><img src=\"" + nbwSetting + "../template/images/telegram.svg\" alt=\"\"></a>\n" +
-        "        <a href=\"\"><img src=\"" + nbwSetting + "../template/images/youtube.svg\" alt=\"\"></a>\n" +
-        "        <a href=\"\"><img src=\"" + nbwSetting + "../template/images/twitter.svg\" alt=\"\"></a>\n" +
-        "        <a href=\"\"><img src=\"" + nbwSetting + "../template/images/instagram.svg\" alt=\"\"></a>\n" +
-        "    </div>\n" +
-        "</div>\n";
-    $("#demo-is-here-n-b-w").html(notif01);
-
+    $(".n-b-w-f-num-01").show();
     body.on("change", "select[name=selectNotification]", function () {
         if ($(this).val() === "introduce_product") {
-            $("#demo-is-here-n-b-w").html(notif01);
-            fillInputs();
+            $(".n-b-w-f-num-02").hide().removeClass("selected-n-b-w");
+            $(".n-b-w-f-num-03").hide().removeClass("selected-n-b-w");
+            $(".n-b-w-f-num-01").show().addClass("selected-n-b-w");
+
+            $(".n-b-w-title-product").show();
+            $(".n-b-w-percent").show();
+            $(".n-b-w-social").hide();
+            $(".n-b-w-price").show();
+            $(".n-b-w-button-title").show();
         }
 
         if ($(this).val() === "day_counter") {
-            $("#demo-is-here-n-b-w").html(notif02);
+            $(".n-b-w-f-num-03").hide().removeClass("selected-n-b-w");
+            $(".n-b-w-f-num-01").hide().removeClass("selected-n-b-w");
+            $(".n-b-w-f-num-02").show().addClass("selected-n-b-w");
+
+            $(".n-b-w-title-product").show();
+            $(".n-b-w-percent").hide();
+            $(".n-b-w-price").hide();
+            $(".n-b-w-social").hide();
+            $(".n-b-w-button-title").show();
         }
 
         if ($(this).val() === "social") {
-            $("#demo-is-here-n-b-w").html(notif03);
+            $(".n-b-w-f-num-01").hide().removeClass("selected-n-b-w");
+            $(".n-b-w-f-num-02").hide().removeClass("selected-n-b-w");
+            $(".n-b-w-f-num-03").show().addClass("selected-n-b-w");
+
+            $(".n-b-w-title-product").show();
+            $(".n-b-w-social").show();
+            $(".n-b-w-percent").hide();
+            $(".n-b-w-price").hide();
+            $(".n-b-w-button-title").hide();
+        }
+    });
+
+    body.on("keyup paste", "input[name=title-product]", () => {
+        let inputValue = $("input[name=title-product]").val();
+        $(".selected-n-b-w").find("#title-product").text(inputValue);
+    });
+    body.on("keyup paste", "input[name=percent]", () => {
+        let inputValue = $("input[name=percent]").val();
+        let selector = $(".selected-n-b-w");
+        selector.find("#percent").text(inputValue);
+        if (!inputValue) selector.find("#percent-parent").hide();
+    });
+    body.on("keyup paste", "input[name=price]", () => {
+        let inputValue = $("input[name=price]").val();
+        let selector = $(".selected-n-b-w").find("#price");
+        if (inputValue) {
+            selector.text(inputValue);
+        }else {
+            selector.hide()
+        }
+    });
+    body.on("keyup paste", "input[name=button-title]", () => {
+        let inputValue = $("input[name=button-title]").val();
+        let selector = $(".selected-n-b-w").find("#button-title");
+        if (inputValue) {
+            selector.text(inputValue);
+        }else {
+            selector.hide();
+        }
+    });
+
+    body.on("keyup paste", "input[name=telegram]", () => {
+        let inputValue = $("input[name=telegram]").val();
+        let selector = $(".selected-n-b-w").find("#telegram");
+        if (inputValue) {
+            selector.attr("href", "https://t.me/" + inputValue).show();
+        } else {
+            selector.hide();
+        }
+    });
+    body.on("keyup paste", "input[name=facebook]", () => {
+        let inputValue = $("input[name=facebook]").val();
+        let selector = $(".selected-n-b-w").find("#facebook");
+        if (inputValue) {
+            selector.attr("href", "https://www.facebook.com/" + inputValue).show();
+        } else {
+            selector.hide();
+        }
+    });
+    body.on("keyup paste", "input[name=twitter]", () => {
+        let inputValue = $("input[name=twitter]").val();
+        let selector = $(".selected-n-b-w").find("#twitter");
+        if (inputValue) {
+            selector.attr("href", "https://twitter.com/" + inputValue).show();
+        } else {
+            selector.hide();
+        }
+    });
+    body.on("keyup paste", "input[name=instagram]", () => {
+        let inputValue = $("input[name=instagram]").val();
+        let selector = $(".selected-n-b-w").find("#instagram");
+        if (inputValue) {
+            selector.attr("href", "https://www.instagram.com/" + inputValue).show();
+        } else {
+            selector.hide();
+        }
+    });
+    body.on("keyup paste", "input[name=youtube]", () => {
+        let inputValue = $("input[name=youtube]").val();
+        let selector = $(".selected-n-b-w").find("#youtube");
+        if (inputValue) {
+            selector.attr("href","https://www.youtube.com/channel/" + inputValue).show();
+        } else {
+            selector.hide();
         }
     });
 });
